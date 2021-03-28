8f915d69e8d0363111a21691067a88e62e342c71
BilalRazzaq22 <81469581+BilalRazzaq22@users.noreply.github.com>
2021-03-28 12:29:36 +0500

    First Commit

---
+++ ERP.WpfClient/ERP.WpfClient/ViewModel/CustomerViewModel.cs
@@ -0,0 +1,55 @@
+﻿using ERP.Common.NotifyProperty;
+using ERP.Entities.DBModel;
+using ERP.Repository.CustomerRepo;
+using ERP.WpfClient.Commands;
+using ERP.WpfClient.Mapper;
+using ERP.WpfClient.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ERP.WpfClient.ViewModel
+{
+    public class CustomerViewModel : ViewModelBase
+    {
+        #region Fields
+
+        //private CustomerRepository _customerRepository;
+        private CustomerModel _customerModel;
+
+        #endregion
+
+        #region Ctor
+
+        public CustomerViewModel()
+        {
+            this.CustomerCommands = new CustomerCommand(this);
+            //_customerRepository = new CustomerRepository();
+            CustomerModel = new CustomerModel();
+        }
+
+        #endregion
+
+        #region Properties
+        public CustomerCommand CustomerCommands { get; set; }
+
+        public CustomerModel CustomerModel
+        {
+            get { return _customerModel; }
+            set { _customerModel = value; RaisePropertyChanged("CustomerModel"); }
+        }
+
+        #endregion
+
+        #region Methods
+
+        public void SaveCustomer()
+        {
+            //_customerRepository.SaveCustomer(MapperProfile.iMapper.Map<Customer>(CustomerModel));
+        }
+
+        #endregion
+    }
+}
